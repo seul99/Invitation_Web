cc7f2520afa34f6ee0096062b02d3ad662ad5bb1
Sia / seulgi <piao516@naver.com>
2025-09-23 15:39:19 +0900

    fix:ios 화면 개선

--- src/style/StyledMain.jsx
+++ src/style/StyledMain.jsx
@@ -25,9 +25,12 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 393px;
+  // width: 393px;
+  width: 100%;
+  max-width: 393px;
   flex-shrink: 0;
   padding-bottom: 20px;
+  overflow-x: hidden;
 `;
 
 export const LetterImg = styled.img`
@@ -35,6 +38,7 @@ export const LetterImg = styled.img`
   z-index: 10;
   top: 50%;
   left: 50%;
+  transform: translate(-50%, -50%); /* ios 위치 수정 */
   animation: ${floatAni} 1.5s infinite ease-in-out;
   filter: drop-shadow(0 0 20px rgba(250, 241, 214, 0.16));
   cursor: pointer;
